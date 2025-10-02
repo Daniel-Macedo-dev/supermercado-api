e6cc340d4644e88832058208a595a6bc6f4ebba9
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2025-10-02 16:55:14 -0300

    add logging to controller layer

--- supermercado-api/src/main/java/com/danielmacedo/supermercado_api/controller/ProdutoController.java
+++ supermercado-api/src/main/java/com/danielmacedo/supermercado_api/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import com.danielmacedo.supermercado_api.dto.ProdutoDTO;
 import com.danielmacedo.supermercado_api.infrastructure.entities.Produto;
 import com.danielmacedo.supermercado_api.mapper.ProdutoMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,14 +16,17 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/produtos")
 @RequiredArgsConstructor
+@Slf4j
 public class ProdutoController {
 
     private final ProdutoService produtoService;
 
     @PostMapping
     public ResponseEntity<ProdutoDTO> criarProduto(@RequestBody ProdutoDTO dto) {
+        log.info("Requisição POST /produtos para criar produtos");
         Produto produto = ProdutoMapper.toEntity(dto);
         Produto salvo = produtoService.save(produto);
+        log.info("Produto criado com id {}", salvo.getId());
         return ResponseEntity.status(HttpStatus.CREATED).body(ProdutoMapper.toDTO(salvo));
     }
 
@@ -44,20 +48,26 @@ public class ProdutoController {
     @PutMapping("/{id}")
     public ResponseEntity<ProdutoDTO> atualizarProduto(@PathVariable Integer id,
                                                        @RequestBody ProdutoDTO dto) {
+        log.info("Requisição PUT /produtos/{} para atualização completa", id);
         Produto produtoAtualizado = produtoService.atualizar(id, ProdutoMapper.toEntity(dto));
+        log.info("Produto atualizado com sucesso", produtoAtualizado.getId());
         return ResponseEntity.ok(ProdutoMapper.toDTO(produtoAtualizado));
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity<ProdutoDTO> atualizarParcial(@PathVariable Integer id,
                                                        @RequestBody ProdutoDTO dto) {
+        log.info("Requisição PATCH /produtos/{} para atualização parcial", id);
         Produto produtoAtualizado = produtoService.atualizar(id, ProdutoMapper.toEntity(dto));
+        log.info("Produto atualizado parcialmente", produtoAtualizado.getId());
         return ResponseEntity.ok(ProdutoMapper.toDTO(produtoAtualizado));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarProduto(@PathVariable Integer id) {
+        log.info("Requisição DELETE /produtos/{}", id);
         produtoService.deletar(id);
+        log.info("Produto criado com id {} deletado com sucesso", id);
         return ResponseEntity.noContent().build();
     }
 }
